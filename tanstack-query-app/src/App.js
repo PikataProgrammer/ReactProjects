6913f1fea7229835f4e1ee46a2f0b86ffdb0b443
PikataProgrammer <petardermendjiev@abv.bg>
2026-02-04 16:43:37 +0200

    Add tanstack-query-app

--- tanstack-query-app/src/App.js
+++ tanstack-query-app/src/App.js
@@ -4,10 +4,13 @@ import {
     createBrowserRouter,
 } from 'react-router-dom';
 
+import { QueryClientProvider } from '@tanstack/react-query';
+
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
+import { queryClient } from './util/http';
 
 const router = createBrowserRouter([
     {
@@ -38,7 +41,11 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
