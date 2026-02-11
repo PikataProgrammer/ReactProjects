ded374a585c7c5934ae735442bd062ae43337e49
PikataProgrammer <petardermendjiev@abv.bg>
2026-02-11 14:47:38 +0200

    Next.js app

---
+++ first-next.js-app/first-next-app/app/meals/[mealSlug]/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import classes from './page.module.css';
+
+export default function MealDetailsPage(){
+    return  (
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image fill ></Image>
+                </div>
+            </header>
+            <main></main>
+        </>
+    );
+}
---
+++ first-next.js-app/first-next-app/components/main-header/main-header.js
@@ -0,0 +1,34 @@
+
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "@/components/main-header/nav-link";
+
+export function MainHeader() {
+
+    return(
+        <>
+            <MainHeaderBackground />
+            <header className={classes.header}>
+                <Link className={classes.logo} href="/public">
+                    <Image src={logoImg} alt="A plate with food on it"/>
+                    NextLevel Food
+                </Link>
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <NavLink href="/meals" >Browse Meals</NavLink>
+                        </li>
+                        <li>
+                            <NavLink href="/community" >Foodies Community</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+
+)
+
+}
